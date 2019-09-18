0db3538395bc29463b33e30331a15d1618afe5ef
naka123 <naka123@kukabarra.info>
2019-09-18 08:28:03 +0600

    fix

--- Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
+++ Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
@@ -246,9 +246,7 @@
 
     #endif // !FYSETC_MINI_12864
 
-  #define BTN_ENC          P0_28   // (58) open-drain
-  #define BTN_EN1          P3_26   // (31) J3-2 & AUX-4
-  #define BTN_EN2          P3_25   // (33) J3-4 & AUX-4
+  #endif // !CR10_STOCKDISPLAY
 
 #endif // HAS_SPI_LCD
 
