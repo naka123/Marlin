2a8d55894d21677b4a464c3d7fbe6b7391e222cc
naka123 <naka123@kukabarra.info>
2020-05-06 06:32:22 +0600

    ubl_segments revert

--- Marlin/src/feature/bedlevel/ubl/ubl_motion.cpp
+++ Marlin/src/feature/bedlevel/ubl/ubl_motion.cpp
@@ -317,7 +317,7 @@
   #if IS_SCARA
     #define DELTA_SEGMENT_MIN_LENGTH 0.25 // SCARA minimum segment size is 0.25mm
   #elif ENABLED(DELTA)
-    #define DELTA_SEGMENT_MIN_LENGTH 0.025 // mm (still subject to DELTA_SEGMENTS_PER_SECOND)
+    #define DELTA_SEGMENT_MIN_LENGTH 0.10 // mm (still subject to DELTA_SEGMENTS_PER_SECOND)
   #else // CARTESIAN
     #ifdef LEVELED_SEGMENT_LENGTH
       #define DELTA_SEGMENT_MIN_LENGTH LEVELED_SEGMENT_LENGTH
